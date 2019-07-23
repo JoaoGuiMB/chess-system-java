b3425e4fcc2434e2877470fda2df081d1e56e16d
João Guilherme <juaogui159@gmail.com>
2019-07-23 14:19:32 -0300

    Position created

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -1,10 +1,15 @@
 package applications;
 
+import boardgame.position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+			
+			position pos = new position(3,5);
+			System.out.println(pos);
+			
+			
 	}
 
 }
