0ce2150e7663fcb640f321277d634cdcbd449a2c
João Guilherme <juaogui159@gmail.com>
2019-07-24 10:05:10 -0300

    BoardException and defensive programing

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,7 +1,5 @@
 package boardgame;
 
-import javax.swing.text.Position;
-
 public class Board {
 	
 	
@@ -10,7 +8,10 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
-		super();
+		if(rows < 1 || columns<1) {
+			throw new BoardException("Error Creating board: there must be at least 1 row and 1 column");
+		}
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece [rows][columns];
@@ -20,31 +21,51 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+	
 
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
+		
 	public Piece piece(int row,int column) {
+		if(!positionExits(row,column)) {
+			throw new BoardException("Position not in the board");
+		}
 		return pieces[row][column];
 	}
 	
-	public Piece piece(boardgame.position position) {
+	public Piece piece(position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not in the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position" + position);
+		}
+		
 		pieces[position.getRow()][position.getColumn()] =  piece;
 		
 		piece.position = position;
 		
+	}
+	
+	private boolean positionExits(int row, int column) {
+		return row>=0 && row < rows && column >=0 && column <columns;
+	}
+	
+	public boolean positionExists(position position) {
+		return positionExits(position.getRow(),position.getColumn() );
+	}
+	public boolean thereIsAPiece(position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not in the board");
+		}
+		
+		return piece(position) != null;
 	}
 	
 	
